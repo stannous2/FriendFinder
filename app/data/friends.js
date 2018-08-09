1803346cf57d38644861b20d30673da2744cdfed
stannous2 <stannous2@yahoo.com>
2018-08-08 21:50:56 -0700

    updates images

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,32 +8,32 @@
 
 let friends = [{
         "name": 'John',
-        'photo': 'https://i.eurosport.com/2015/10/10/1708800-36174403-2560-1440.jpg?w=700',
+        'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvZvLNos9AAwy_exEHYJMHLcIPTj1fMi8WPMQRqq3MyaGBYcKiVA',
         'scores': [5, 1, 2, 3, 4, 5, 3, 5, 4, 5]
     },
     {
         "name": 'Mike',
-        'photo': 'https://s.yimg.com/ny/api/res/1.2/XSdBjdHwgmdLuG1UYClivQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAwO2lsPXBsYW5l/https://media.zenfs.com/creatr-images/GLB/2018-07-20/f3b4e1c0-8c20-11e8-bc15-d97653969cae_mbappe-4.jpg',
+        'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5t99qB8QI-t2hHyDBbp71gEsSliIDoMKOtFf1bWZYyk_45MFk',
         'scores': [5, 4, 4, 3, 4, 5, 3, 5, 4, 5]
     },
     {
         "name": 'Solo',
-        'photo': 'http://www.trbimg.com/img-56cc93a4/turbine/redeye-the-7-highest-paid-womens-soccer-players-20160223',
+        'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVUS4W9Prtxb5ljvovUmz_pXy54wyrsSJpkYzcHTJHI2mjtuD4Dw',
         'scores': [5, 2, 1, 3, 4, 2, 4, 5, 5, 5]
     },
     {
         "name": 'Shasa',
-        'photo': 'https://imgc.allpostersimages.com/img/posters/liverpool-salah-17-18_u-L-F959V60.jpg?src=gp&w=300&h=375',
+        'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkHXcEEoaIH2F6Wo0I8791c_lRbJ6wa5zaP7Gmi6iTKt_VMg_Y',
         'scores': [5, 3, 2, 3, 4, 5, 3, 5, 4, 5]
     },
     {
-        "name": 'Zane',
-        'photo': 'https://assets.vogue.com/photos/5b212987f90cc35fe4d59610/master/pass/01-story-world-cup-soccer-players-to-watch.jpg',
+        "name": 'Kylian',
+        'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF6s4wzpkhVZtZIh0AqvpxO6_76kLHGMYNpGBkE4QJZblx4BIZ',
         'scores': [5, 1, 5, 5, 1, 2, 4, 1, 2, 4]
     },
     {
-        "name": "brandon",
-        "photo": "https://www.denverpost.com/wp-content/uploads/2017/04/sebastian-giovinco.jpg?w=563",
+        "name": "Jenny",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3akvZ78rGCAQrVaPWqknZ0ipN524ROSpt3oyzeEpO-3uFa82D",
         "scores": [5, 3, 1, 1, 2, 5, 2, 4, 3, 5 ]
     }
 ]
