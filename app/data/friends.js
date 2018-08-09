08e296af8893d935ff246acb9e7f2285d23f8f61
stannous2 <stannous2@yahoo.com>
2018-08-08 17:12:05 -0700

    new files

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of friends.
+// Initially we just set it equal to a "dummy" friend.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+
+let friends = [
+    {
+        "name": 'John',
+        'photo': 'http://media.licdn.com/mpr/mpr/12345.jpg',
+        'scores': [5, 1, 2, 3, 4, 5, 3, 5, 4, 5]
+    },
+    {
+        "name": 'Mike',
+        'photo': 'http://media.licdn.com/mpr/mpr/22345.jpg',
+        'scores': [5, 4, 4, 3, 4, 5, 3, 5, 4, 5]
+    },
+    {
+        "name": 'Pete',
+        'photo': 'http://media.licdn.com/mpr/mpr/22345.jpg',
+        'scores': [5, 2, 1, 3, 4, 2, 4, 5, 5, 5]
+    },
+    {
+        "name": 'Shasa',
+        'photo': 'http://media.licdn.com/mpr/mpr/12345.jpg',
+        'scores': [5, 3, 2, 3, 4, 5, 3, 5, 4, 5]
+    },
+    {
+        "name": 'Zane',
+        'photo': 'http://media.licdn.com/mpr/mpr/12345.jpg',
+        'scores': [5, 1, 5, 5, 1, 2, 4,1, 2, 4]
+    },
+]
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// const path = require("path");
+// const apiRoutes = require("./routing/apiRoutes");
+// const htmlRoutes = require("./routing/htmlRoutes");
+// let friends = require("./app/data/friends");
+
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+
+// sets an initial port for server to listen on
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+//setup routes
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+// Listner
+app.listen(PORT, function (err) {
+    if (err) throw err;
+    console.log(`You're now running a server on port ${PORT}`)
+})
